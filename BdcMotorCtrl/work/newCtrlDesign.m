436139c40cc8f27da12f880a705e45340225a5c5
clmha <45580177+clmha@users.noreply.github.com>
2020-01-25 16:38:18 -0500

    MCU Subsystem and Controller Tuning
    Regroups the Controller and Amplifier subsystems into a MCU (Motor Control Unit) subsystem. Takes also the opportunity to replace scope blocks for viewers in the signal and scope manager to avoid spagethi model.
    
    Adds a scope view dedicated to the controller (command, measurements, effort).
    
    Tunes the controller (modeled using a continuous transfer function) with the newCtrlDesign.m script.
    
    Adds to the .gitignore the *.asv created by MATLAB.

---
+++ BdcMotorCtrl/work/newCtrlDesign.m
@@ -0,0 +1,37 @@
+%% New Controller Design
+
+%% Introduction
+mdl = 'top_model';
+load_system(mdl);
+h = get_param(mdl, 'modelworkspace');
+
+%% Parameters
+R = [2, 1.8, 2.2]; % (Ohm)
+J = [.02, .03, .01]; % (kg.m2/s2)
+[R, J] = ndgrid(R, J);
+
+%% Linearization
+params(1).Name = 'R';
+params(1).Value = R;
+params(2).Name = 'J';
+params(2).Value = J;
+io = getlinio(mdl);
+[linsys, linop] = linearize(mdl, io, params);
+
+%% Controller Design
+% Starting from the current controller implemented in the Simulink model we
+% start the SISO Control System Designer.
+%
+% Current requirements are:
+%  # GM > 20 dB
+%  # PM > 30 deg
+%  # Rise time at 80% < 1 s 
+%  # Settling time at 1 % < 5 s
+%  # Overshoot < 10 %
+%
+% After tuning the controller, do not forget to export your design and
+% update the Simulink model.
+num = getVariable(h, 'ctrlNum');
+den = getVariable(h, 'ctrlDen');
+C = tf(num, den);
+controlSystemDesigner(linsys, C);
