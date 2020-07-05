d38994d528d732b850f4d0031622406f2070f251
clmha <45580177+clmha@users.noreply.github.com>
2020-07-04 20:27:02 -0400

    Adds the ElectricalModelLib to the Simulink Library Browser

---
+++ PmsmMotorCtrl/models/slblocks.m
@@ -0,0 +1,6 @@
+function blkStruct = slblocks
+%SLBLOCKS Specifies that the library should appear in the Library Browser
+
+Browser.Library = 'ElectricalModelLib';
+Browser.Name = 'Electrical';
+blkStruct.Browser = Browser;
